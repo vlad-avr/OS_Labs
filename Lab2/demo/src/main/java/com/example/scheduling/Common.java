0028951a341113f768192efc8b92068d73256490
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-11-11 12:40:08 +0200

    got rid of useless loops

--- Lab2/demo/src/main/java/com/example/scheduling/Common.java
+++ Lab2/demo/src/main/java/com/example/scheduling/Common.java
@@ -2,7 +2,7 @@ package com.example.scheduling;
 
 public class Common {
 
-  static public int s2i (String s) {
+  static public int s2i(String s) {
     int i = 0;
 
     try {
@@ -13,24 +13,26 @@ public class Common {
     return i;
   }
 
-  static public double R1 () {
+  static public double R1() {
     java.util.Random generator = new java.util.Random(System.currentTimeMillis());
     double U = generator.nextDouble();
-    while (U < 0 || U >= 1) {
-      U = generator.nextDouble();
-    }
+    U = generator.nextDouble();
     double V = generator.nextDouble();
-    while (V < 0 || V >= 1) {
-      V = generator.nextDouble();
+    V = generator.nextDouble();
+    double X = Math.sqrt((8 / Math.E)) * (V - 0.5) / U;
+    if (!(R2(X, U))) {
+      return -1;
     }
-    double X =  Math.sqrt((8/Math.E)) * (V - 0.5)/U;
-    if (!(R2(X,U))) { return -1; }
-    if (!(R3(X,U))) { return -1; }
-    if (!(R4(X,U))) { return -1; }
+    if (!(R3(X, U))) {
+      return -1;
+    }
+    if (!(R4(X, U))) {
+      return -1;
+    }
     return X;
   }
 
-  static public boolean R2 (double X, double U) {
+  static public boolean R2(double X, double U) {
     if ((X * X) <= (5 - 4 * Math.exp(.25) * U)) {
       return true;
     } else {
@@ -38,7 +40,7 @@ public class Common {
     }
   }
 
-  static public boolean R3 (double X, double U) {
+  static public boolean R3(double X, double U) {
     if ((X * X) >= (4 * Math.exp(-1.35) / U + 1.4)) {
       return false;
     } else {
@@ -46,7 +48,7 @@ public class Common {
     }
   }
 
-  static public boolean R4 (double X, double U) {
+  static public boolean R4(double X, double U) {
     if ((X * X) < (-4 * Math.log(U))) {
       return true;
     } else {
@@ -55,4 +57,3 @@ public class Common {
   }
 
 }
-
