5cf3521bcf5c9a25fc5178be30eec1e6754b22b7
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-11-17 19:22:52 +0200

    added basics to User

--- Lab2/demo/src/main/java/com/example/scheduling/User.java
+++ Lab2/demo/src/main/java/com/example/scheduling/User.java
@@ -1,5 +1,12 @@
 package com.example.scheduling;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
-    
+    public List<sProcess> processes = new ArrayList<>();
+    public int weight;
+    public User(int weight){
+        this.weight = weight;
+    }
 }
